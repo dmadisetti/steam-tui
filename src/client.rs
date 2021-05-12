606c893c4e2fde7ea5471e3b7049da91ae4fa018
madisetti <madisetti@jhu.edu>
2021-05-11 20:21:22 -0400

    Added actual non-unicode character

--- src/client.rs
+++ src/client.rs
@@ -93,9 +93,6 @@ fn execute(
                     let mut updated = 0;
                     let waiting = queue.len();
                     if let Ok(buf) = cmd.next() {
-                        if let Err(err) = std::str::from_utf8(&buf) {
-                            eprintln!("err: {}\n buf : {:?}", err, buf);
-                        }
                         let response = String::from_utf8_lossy(&buf);
                         match INPUT_LEX.tokenize(&line).as_slice() {
                             &["login", _] => {
@@ -149,10 +146,13 @@ fn execute(
                                         {
                                             for wrapper in apps.values() {
                                                 if let Datum::Value(id) = wrapper {
-                                                    queue.push_front(Command::Cli(format!(
-                                                        "app_info_print {}\n",
-                                                        id
-                                                    )));
+                                                    let key = id.parse::<i32>().unwrap_or(-1);
+                                                    if key >= 0 {
+                                                        queue.push_front(Command::Cli(format!(
+                                                            "app_info_print {}\n",
+                                                            key
+                                                        )));
+                                                    }
                                                 }
                                             }
                                         }
@@ -319,7 +319,7 @@ mod tests {
         let (tx1, receiver) = channel();
         let (sender, rx2) = channel();
         Client::start_process(Arc::new(Mutex::new(State::LoggedOut)), tx1, rx2);
-        let pollution = String::from("pollution ™️ ö ®Ø 天 🎉\n");
+        let pollution = String::from("pollution ™️ ö ®Ø 天 🎉 Maxisâ¢\n");
         sender
             .send(Command::Cli(pollution.clone()))
             .expect("Fails to send message...");
